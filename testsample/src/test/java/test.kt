932a0980ab7d5da0c27f6222346a080eedcbb6d9
rhems234 <rhems234@gmail.com>
2023-05-10 09:33:02 +0900

    230510 1교시 저장 1

---
+++ testsample/src/test/java/test.kt
@@ -0,0 +1,15 @@
+// 최상위 영역
+// java : int num = 1;
+// 코트린 : val(or var) 변수명 : 타입 = 값
+// 깃 사용시 주의사항.
+// 1. 학원, 2.집, 3. 깃 원격지
+// 항상 어디가 최신인지 알아야함.
+val num : Int = 1
+
+class test {
+}
+
+fun main() {
+    println("Hello World")
+    println("num의 값 : $num")
+}
